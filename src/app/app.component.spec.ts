879448ed340215c634fdc1aa5700d2b875999a0a
jaswanth <jaswanthn621@gmail>
2023-02-07 07:59:09 +0530

    my commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'first-angular'`, () => {
+  it(`should have as title 'besant'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('first-angular');
+    expect(app.title).toEqual('besant');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('first-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('besant app is running!');
   });
 });
